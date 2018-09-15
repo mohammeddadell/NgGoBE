80d70d21cb2d5ec6399b7407054867eb78bc2a6e
Hamster <clemensgrabow@gmx.de>
2018-09-15 19:37:46 +0200

    socket integration

--- src/app.js
+++ src/app.js
@@ -3,12 +3,18 @@ import { env, mongo, port, ip, apiRoot } from './config'
 import mongoose from './services/mongoose'
 import express from './services/express'
 import api from './api'
+import SocketIO from 'socket.io'
 
 const app = express(apiRoot, api)
 const server = http.createServer(app)
+const io = new SocketIO(server) // server.server
 
 mongoose.connect(mongo.uri)
 mongoose.Promise = Promise
+
+io.on('connection', (socket) => {
+  console.log('SOCKET CONNECTED')
+})
 
 setImmediate(() => {
   server.listen(port, ip, () => {
